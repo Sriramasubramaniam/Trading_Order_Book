3f930d5d1b822d92740e281a16e81a39ce9e4116
Sriram <sriram@lionsbot.com>
2024-02-06 20:32:48 +0530

    feat:basic html renderer without much focus on css

--- src/render/OrderBookRenderer.tsx
+++ src/render/OrderBookRenderer.tsx
@@ -24,7 +24,51 @@ const OrderBookRenderer = () => {
         //write a clean up function later
     }, []);
 
-    return <div>Order book</div>;
+    return (
+        <div style={{ display: "flex" }}>
+            <table style={{ border: "2px solid green" }}>
+                <thead>
+                    <tr>
+                        <th style={{ width: "130px", fontSize: "18px" }}>Count</th>
+                        <th style={{ width: "130px", fontSize: "18px", textAlign: "right" }}>
+                            Amount
+                        </th>
+                        <th style={{ width: "130px", fontSize: "18px" }}>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.entries(bids).map(([price, { count, amount }]) => (
+                        <tr key={price}>
+                            <td style={{ width: "130px", textAlign: "center" }}>{count}</td>
+                            <td style={{ width: "130px", textAlign: "right" }}>{amount}</td>
+                            <td style={{ width: "130px", textAlign: "center" }}>{price}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <table style={{ border: "2px solid red" }}>
+                <thead>
+                    <tr>
+                        <th style={{ width: "130px", fontSize: "18px" }}>Price</th>
+                        <th style={{ width: "130px", fontSize: "18px", textAlign: "right" }}>
+                            Amount
+                        </th>
+                        <th style={{ width: "130px", fontSize: "18px" }}>Count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.entries(asks).map(([price, { count, amount }]) => (
+                        <tr key={price}>
+                            <td style={{ width: "130px", textAlign: "center" }}>{price}</td>
+                            <td style={{ width: "130px", textAlign: "right" }}>{amount}</td>
+                            <td style={{ width: "130px", textAlign: "center" }}>{count}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default OrderBookRenderer;
